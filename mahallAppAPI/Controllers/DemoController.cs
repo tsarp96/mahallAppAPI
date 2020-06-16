9a0bf127a85e0a08bfdeee8ff306ebc91de84743
tsarp96 <tsarp96@hotmail.com>
2020-06-17 01:01:44 +0300

    denemeler v2

--- mahallAppAPI/Controllers/DemoController.cs
+++ mahallAppAPI/Controllers/DemoController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace mahallAppAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("demo")]
     [ApiController]
     public class DemoController : ControllerBase
     {
