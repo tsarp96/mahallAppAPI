d63bc2890ea0c9acdc39ea60462a98d012cb0e61
tsarp96 <tsarp96@hotmail.com>
2020-06-17 00:48:56 +0300

    add demoController

--- mahallAppAPI/Controllers/DemoController.cs
+++ mahallAppAPI/Controllers/DemoController.cs
@@ -10,35 +10,35 @@ namespace mahallAppAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class DemoController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        // GET: api/<DemoController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<DemoController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<ValuesController>
+        // POST api/<DemoController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<DemoController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/<DemoController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
