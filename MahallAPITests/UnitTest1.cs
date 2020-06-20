1a88c5e6aa44bfb5bf2a6abea45d01cf02f41bc6
tsarp96 <tsarp96@hotmail.com>
2020-06-21 00:13:38 +0300

    Initial Unit Test

---
+++ MahallAPITests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace MahallAPITests
+{
+    public class UnitTest1
+    {
+
+        // TODO: Kullanıcı adı dolu mu ?
+        // TODO: Şıfre dogru mu
+        // TODO: Sıfreyı hashle
+        // TODO: Kullanıcı var mı? yoksa hata 
+        // TODO: Kullanıcı bılgılırını don
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
